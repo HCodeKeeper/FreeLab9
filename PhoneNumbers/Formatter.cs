58dc89c57f09f94093eb7a5bfc223b9b697197b2
HCodeKeeper <ivannikovmax04@gmail.com>
2021-12-15 06:02:39 +0200

    Done

--- PhoneNumbers/Formatter.cs
+++ PhoneNumbers/Formatter.cs
@@ -10,20 +10,19 @@ namespace PhoneNumbers
 {
     public class Formatter
     {
-        private int FullPhoneNumberSize;
         private string Pattern;
         private readonly string DefaultHeader; //First line that will be appended before the formatted data //Просто что бы разграничивать
 
         public Formatter()
         {
-            FullPhoneNumberSize = 12;
             Pattern = @"^(\+38|3?8?)(\(|\s)?\d{3}(\)\s?|\s)?\d{3}(-?\d{2}){2}$"; //@ - дает возможность записывать строку без знаков экранированя "\", в следствии чего не приходится использовать двойной \ 
-            DefaultHeader = "[Output]";
+            DefaultHeader = "\n[Output]";
         }
         
 
         public void Format(string sourcePath, string destination, string header)
         {
+            int WithoutCountryCodeSize = 10; //не учитывая 38
             List<string> data = GetData(sourcePath);
             for (int i=0; i < data.Count; i++)
             {
@@ -36,14 +35,19 @@ namespace PhoneNumbers
                 StringBuilder sb = new StringBuilder();
                 foreach (char c in data[i].ToCharArray())
                 {
-                    if (Regex.IsMatch(c.ToString(), "\\d"))
+                    if (Regex.IsMatch(c.ToString(), "\\d")) //фильтруем и генерим стринг билдер только из цифр номера
                     {
                         sb.Append(c);
                     }
                 }
 
-                int beginInd = FullPhoneNumberSize - sb.Length; // с этого индекса будет начинатся часть номера, полученного из входных данных
+                int beginInd = sb.Length - WithoutCountryCodeSize; // с этого индекса будет начинаться номер без 38
+                string resultLine = "+38 " + sb.ToString(beginInd, 3) + sb.ToString(beginInd + 3, 3) // 38 + *** + *** + ** + **
+                                    + "-" + sb.ToString(beginInd + 6, 2) + "-" + sb.ToString(beginInd + 8, 2);
+                data[i] = resultLine;
+                
             }
+            WriteData(destination, GetWithHeader(data, header));
         }
 
         public void Format(string sourcePath, string destination)
@@ -51,9 +55,9 @@ namespace PhoneNumbers
             Format(sourcePath, destination, DefaultHeader);
         }
 
-        private List<string> GetWithHeader(List<string> data, string header)
+        private List<string> GetWithHeader(List<string> data, string header) //в стилистических целях добавляем строку отделающуюю старые и новые данные в файле
         {
-            List<string> headedData = new List<string>(data);
+            List<string> headedData = new List<string>(data); //избавляемся от потенциальных проблем с мутабельностью списков
             headedData.Insert(0, header);
             return headedData;
         }
@@ -62,12 +66,13 @@ namespace PhoneNumbers
         {
             List<string> data = new List<string>();
             
-            using (StreamReader streamReader = new StreamReader(sourcePath))
+            using (StreamReader streamReader = new StreamReader(sourcePath)) //используем using resources что бы неявно закрыть поток после выхода из scope'а using
+            //могут быть memore leak'и так как исключения не обработаны
             {
                 while (true)
                 {
-                    string? line = streamReader.ReadLine();
-                    if (line == null)
+                    string? line = streamReader.ReadLine(); //очень важно сформировать список строк для удобности и из-за использования ^abc$ в паттерне
+                    if (line == null) //если достигли конца файла
                     {
                         break;
                     }
@@ -79,7 +84,7 @@ namespace PhoneNumbers
             return data;
         }
 
-        private void WriteData(string destination, string[] data)
+        private void WriteData(string destination, List<string> data)
         {
             using (StreamWriter streamWriter = new StreamWriter(destination, true))
             {
--- PhoneNumbers/Program.cs
+++ PhoneNumbers/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+//by HCodeKeeper
 namespace PhoneNumbers
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            Formatter fm = new Formatter();
             //удалить ненужные комменты!
+            //Exception'ы не были реализованы. Советую запускать в try-catch
+            fm.Format("YOUR_PATH", "YOUR_PATH");
         }
     }
 }
